88c1c669e7f9f2904c50ec3244c88adfe57e30d6
Orie Steele <orie@transmute.industries>
2018-07-28 00:01:52 -0500

    add functions express example

---
+++ functions/README.md
@@ -0,0 +1,10 @@
+
+### Getting Started
+
+
+```
+npm i
+npm run serve
+```
+
+http://localhost:3002/mypass-atx/us-central1/widgets
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+// app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+app.get('/', (req, res) =>
+  res.json({
+    yolo: 1
+  })
+);
+
+// Expose Express API as a single Cloud Function:
+exports.widgets = functions.https.onRequest(app);
